84cabbdfedd97041962b537f6909fc5220121d4b
root <root@git.maharshi.ninja>
2021-11-06 01:23:10 +0530

    Update PWA settings

--- vue.config.js
+++ vue.config.js
@@ -2,6 +2,19 @@ module.exports = {
   transpileDependencies: [
     'vuetify'
   ],
+  pwa: {
+    workboxOptions: {
+      navigateFallback: 'index.html',
+      skipWaiting: true,
+      importWorkboxFrom: 'local',
+      runtimeCaching: [
+        {
+          urlPattern: /\.(?:png|svg|ico)$/,
+          handler: 'CacheFirst'
+        }
+      ]
+    }
+  },
 
   lintOnSave: false
 }
