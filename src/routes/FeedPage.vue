c4b74fb5880a2aa8bf8faf1365df47797673b14e
root <root@git.maharshi.ninja>
2021-11-14 01:56:10 +0530

    Use a 6-column grid for the Feed Pages (Subscriptions and Trending)

--- src/routes/FeedPage.vue
+++ src/routes/FeedPage.vue
@@ -16,8 +16,8 @@
       <span v-if="!errorIsJSON">{{ $t(error) }}</span>
       <JSONViewer v-else :value="error" />
     </v-alert>
-    <v-row v-for="(row, rowId) in chunkedByFour" :key="rowId">
-      <v-col md="3" v-for="(video, videoId) in row" :key="videoId">
+    <v-row v-for="(row, rowId) in chunkedBySix" :key="rowId">
+      <v-col md="2" v-for="(video, videoId) in row" :key="videoId">
         <VideoItem :video="video" max-height />
       </v-col>
     </v-row>
@@ -102,8 +102,8 @@ export default {
     }
   },
   computed: {
-    chunkedByFour () {
-      return _chunk(this.videos, 4)
+    chunkedBySix () {
+      return _chunk(this.videos, 6)
     }
   },
   components: {
